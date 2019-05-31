9fc303604bd6a2fc810983a57d80d71b4f9715bb
Jeciel Barraza <bjecielz@gmail.com>
2019-05-31 11:01:08 -0500

    Update AttributeParser.cpp

--- AttributeParser.cpp
+++ AttributeParser.cpp
@@ -3,10 +3,39 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <sstream>
+
 using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N=4, Q= 3;
+    //cin>>N>>Q;
+    
+    vector<vector<string>> tags; 
+
+    string texto[7] = {"<tag1 value = \"HelloWorld\">", "<tag2 name = \"Name1\">","</tag2>", "</tag1>","tag1.tag2~name","tag1~name", "tag1~value"};
+    
+    string linea;
+    for(int i = 0; i<N; i++){
+        stringstream lin(texto[i]); 
+        string word;
+        
+        while(lin>>word){
+           
+            
+            if(word.find('<') == 0) //tag
+                cout<<"tag"<<endl;
+            
+            if(word.find('>') == )
+                cout<<"closing tag"<<endl;
+            
+        }
+        
+        //The following  lines consist of either an opening tag with zero or more attributes or a closing tag.
+        //There is a space after the tag-name, attribute-name, '=' and value.There is no space after the last value. 
+        //If there are no attributes there is no space after tag name.
+    }
+    
     return 0;
 }
